2b82c169587fc66a4dadcd46d5d90fba5dd4502a
Usman Khalid <usmankhalid5423@gmail.com>
2025-07-09 11:11:19 +0400

    WebRTC Admin panel

---
+++ Signal/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Signal.Models;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Signal.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration; // Injecting IConfiguration
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            string storedUsername = _configuration["LoginCredentials:Username"];
+            string storedPassword = _configuration["LoginCredentials:Password"];
+            if (ModelState.IsValid)
+            {
+                if (model.Username == storedUsername && model.Password == storedPassword) // Simple example
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewData["ErrorMessage"] = "Invalid username or password.";
+                }
+            }
+            return View("Index", model);
+        }
+
+    }
+}
